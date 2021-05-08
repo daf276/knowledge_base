0fc5ed3244d68fa824f1aa278090dd3c52222a98
Attila <daf276@gmx.de>
2021-05-08 19:02:41 +0200

    Now using a minimal example theme

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import {ThemeProvider} from '@material-ui/core/styles';
+import theme from './theme';
 
 ReactDOM.render(
-    <App />,
+    <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <App/>
+    </ThemeProvider>,
     document.querySelector('#root'),
 );

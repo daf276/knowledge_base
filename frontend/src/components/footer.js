6a7cdc175373182ab5232c4fe0432f0c16aca645
Attila <daf276@gmx.de>
2021-05-08 19:04:28 +0200

    Example footer

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -1,20 +1,32 @@
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import {makeStyles} from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Button from "@material-ui/core/Button";
 
-class Header extends React.Component {
-    render() {
-        return (
-            <AppBar position="relative">
-                <Toolbar>
-                    <Typography variant="h6" color="inherit" noWrap>
-                        Knowledge Base
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-        );
-    }
+const useStyles = makeStyles((theme) => ({
+    footer: {
+        padding: theme.spacing(3, 2),
+        marginTop: 'auto',
+        backgroundColor:
+            theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
+    },
+}));
+
+export default function StickyFooter() {
+    const classes = useStyles();
+
+    return (
+        <div>
+            <CssBaseline/>
+            <footer className={classes.footer}>
+                <Container style={{display: 'flex', justifyContent: 'right'}}>
+                    <Button size="small" color="primary"
+                            onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})}>
+                        Back to top
+                    </Button>
+                </Container>
+            </footer>
+        </div>
+    );
 }
-
-export default Header;

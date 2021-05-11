8f36ae079501db3142e010e83327150db28355c1
Attila <daf276@gmx.de>
2021-05-11 19:34:11 +0200

    Added home button to header

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -2,15 +2,28 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Grid from "@material-ui/core/Grid";
+import {Link} from "react-router-dom";
 
 class Header extends React.Component {
     render() {
         return (
             <AppBar position="relative">
                 <Toolbar>
-                    <Typography variant="h6" color="inherit" noWrap>
-                        Knowledge Base
-                    </Typography>
+                    <Grid container spacing={2}>
+                            <Grid item xs={4}>
+                                <Link to="/">
+                                <Typography variant="h6" noWrap>
+                                    Return to Main Page
+                                </Typography>
+                                </Link>
+                            </Grid>
+                        <Grid item xs={4}>
+                            <Typography variant="h6" align="center" noWrap>
+                                Knowledge Base
+                            </Typography>
+                        </Grid>
+                    </Grid>
                 </Toolbar>
             </AppBar>
         );
